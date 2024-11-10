be4817a8b3b5951da9416fa41a9e08e816ec6f67
Ernie1234 <anumahjoshuaeneye@gmail.com>
2024-11-10 19:25:34 +0100

    feat: profile page in ui progress

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,10 +1,22 @@
 import TopNav from "@/components/shared/TopNav";
+import Image from "next/image";
 
 export default function page() {
   return (
-    <main>
+    <main className="min-h-dvh">
       <TopNav title="Profile" />
-      profile page
+      <div className="flex bg-orange-100 p-4">
+        <div className="rounded-full w-12 h-12">
+          <Image
+            alt="profile-image"
+            src=""
+            className="w-full object-cover"
+            width={500}
+            height={500}
+          />
+        </div>
+        <h1 className="text-xl">Elegance by enoila</h1>
+      </div>
     </main>
   );
 }
